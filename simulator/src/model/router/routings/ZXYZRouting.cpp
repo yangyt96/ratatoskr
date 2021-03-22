ea4614019b86076cbea94b09de96f551290bbf93
yangyt96 <yee.yang.tan@rwth-aachen.de>
2021-03-22 22:09:26 +0100

    [feat] 3d torus ZXYZ routing algorithm

--- simulator/src/model/router/routings/ZXYZRouting.cpp
+++ simulator/src/model/router/routings/ZXYZRouting.cpp
@@ -28,29 +28,75 @@ int ZXYZRouting::route(int src_node_id, int dst_node_id)
     Vec3D<float> dst_pos = globalResources.nodes.at(dst_node_id).pos;
     int con_pos = -1;
 
-    if (dst_pos==src_pos) {
-        con_pos = src_node.getConPosOfDir(DIR::Local);
-    } else if (dst_pos.z == .5 && src_pos.z == .5){
-        con_pos = src_node.getConPosOfDir(DIR::Down);
+    if (!globalResources.routingCircular)
+    {
+        if (dst_pos == src_pos)
+            con_pos = src_node.getConPosOfDir(DIR::Local);
+        else if (dst_pos.z == .5 && src_pos.z == .5)
+            con_pos = src_node.getConPosOfDir(DIR::Down);
+        else if (dst_pos.z < src_pos.z)
+            con_pos = src_node.getConPosOfDir(DIR::Down);
+        else if (dst_pos.z >= src_pos.z)
+        {
+            if (dst_pos.x < src_pos.x)
+                con_pos = src_node.getConPosOfDir(DIR::West);
+            else if (dst_pos.x > src_pos.x)
+                con_pos = src_node.getConPosOfDir(DIR::East);
+            else if (dst_pos.y < src_pos.y)
+                con_pos = src_node.getConPosOfDir(DIR::South);
+            else if (dst_pos.y > src_pos.y)
+                con_pos = src_node.getConPosOfDir(DIR::North);
+            else
+                con_pos = src_node.getConPosOfDir(DIR::Up);
+        }
     }
-    else if (dst_pos.z<src_pos.z) {
-        con_pos = src_node.getConPosOfDir(DIR::Down);
-    }
-    else if (dst_pos.z>=src_pos.z) {
-        if (dst_pos.x<src_pos.x) {
-            con_pos = src_node.getConPosOfDir(DIR::West);
-        }
-        else if (dst_pos.x>src_pos.x) {
-            con_pos = src_node.getConPosOfDir(DIR::East);
-        }
-        else if (dst_pos.y<src_pos.y) {
-            con_pos = src_node.getConPosOfDir(DIR::South);
-        }
-        else if (dst_pos.y>src_pos.y) {
-            con_pos = src_node.getConPosOfDir(DIR::North);
-        }
-        else
+    else
+    {
+        Vec3D<int> src_coord = globalResources.norm_to_coord[src_pos];
+        Vec3D<float> inner_len = (src_pos - dst_pos).abs();
+        Vec3D<float> outer_len;
+
+        outer_len.x = 1. - inner_len.x + globalResources.x_step[src_coord.z];
+        outer_len.y = 1. - inner_len.y + globalResources.y_step[src_coord.z];
+        outer_len.z = 1. - inner_len.z + globalResources.z_step;
+
+        if (dst_pos == src_pos)
+            con_pos = src_node.getConPosOfDir(DIR::Local);
+        else if (dst_pos.z == .5 && src_pos.z == .5)
+            con_pos = src_node.getConPosOfDir(DIR::Down);
+        //  z-axis
+        else if ((dst_pos.z < src_pos.z) && (inner_len.z <= outer_len.z))
+            con_pos = src_node.getConPosOfDir(DIR::Down);
+        else if ((dst_pos.z < src_pos.z) && (inner_len.z >= outer_len.z))
             con_pos = src_node.getConPosOfDir(DIR::Up);
+        else if (dst_pos.z >= src_pos.z)
+        {
+            // x-axis
+            if ((dst_pos.x < src_pos.x) && (inner_len.x <= outer_len.x))
+                con_pos = src_node.getConPosOfDir(DIR::West);
+            else if ((dst_pos.x < src_pos.x) && (inner_len.x >= outer_len.x))
+                con_pos = src_node.getConPosOfDir(DIR::East);
+            else if ((dst_pos.x > src_pos.x) && (inner_len.x <= outer_len.x))
+                con_pos = src_node.getConPosOfDir(DIR::East);
+            else if ((dst_pos.x > src_pos.x) && (inner_len.x >= outer_len.x))
+                con_pos = src_node.getConPosOfDir(DIR::West);
+            // y-axis
+            else if ((dst_pos.y < src_pos.y) && (inner_len.y <= outer_len.y))
+                con_pos = src_node.getConPosOfDir(DIR::South);
+            else if ((dst_pos.y < src_pos.y) && (inner_len.y >= outer_len.y))
+                con_pos = src_node.getConPosOfDir(DIR::North);
+            else if ((dst_pos.y > src_pos.y) && (inner_len.y <= outer_len.y))
+                con_pos = src_node.getConPosOfDir(DIR::North);
+            else if ((dst_pos.y > src_pos.y) && (inner_len.y >= outer_len.y))
+                con_pos = src_node.getConPosOfDir(DIR::South);
+            //  z-axis
+            else if ((dst_pos.z > src_pos.z) && (inner_len.z <= outer_len.z))
+                con_pos = src_node.getConPosOfDir(DIR::Up);
+            else if ((dst_pos.z > src_pos.z) && (inner_len.z >= outer_len.z))
+                con_pos = src_node.getConPosOfDir(DIR::Down);
+            else
+                con_pos = src_node.getConPosOfDir(DIR::Up);
+        }
     }
 
     return con_pos;
